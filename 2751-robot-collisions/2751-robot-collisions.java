c485ba84262c0507b9d3c104f184544407cfa1f3
Ram Thokane <thokaneram@gmail.com>
2026-04-02 00:28:41 +0530

    Time: 27 ms (96.52%), Space: 109.6 MB (93.91%) - LeetHub

---
+++ 2751-robot-collisions/2751-robot-collisions.java
@@ -0,0 +1,38 @@
+class Solution {
+    public List<Integer> survivedRobotsHealths(int[] positions, int[] healths, String directions) {
+        int n = positions.length;
+        Integer[] indices = new Integer[n];
+        for (int i = 0; i < n; i++) indices[i] = i;
+
+        Arrays.sort(indices, (a, b) -> Integer.compare(positions[a], positions[b]));
+
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int currentIndex : indices) {
+            if (directions.charAt(currentIndex) == 'R') {
+                stack.push(currentIndex);
+            } else {
+                while (!stack.isEmpty() && healths[currentIndex] > 0) {
+                    int topIndex = stack.pop();
+
+                    if (healths[currentIndex] > healths[topIndex]) {
+                        healths[currentIndex] -= 1;
+                        healths[topIndex] = 0;
+                    } else if (healths[currentIndex] < healths[topIndex]) {
+                        healths[topIndex] -= 1;
+                        healths[currentIndex] = 0;
+                        stack.push(topIndex);
+                    } else {
+                        healths[currentIndex] = 0;
+                        healths[topIndex] = 0;
+                    }
+                }
+            }
+        }
+
+        List<Integer> result = new ArrayList<>();
+        for (int h : healths) {
+            if (h > 0) result.add(h);
+        }
+        return result;
+    }
+}
